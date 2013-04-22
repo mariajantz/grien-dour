9a44cc3bea2d4d55c37803c853ab1b0c298df9fa
Maria Jantz <mariajantz@Maria-Jantzs-MacBook-Pro.local>
2013-04-22 19:59:33 -0400

    greendoor.py

--- greendoor.py
+++ greendoor.py
@@ -28,6 +28,7 @@ class Game:
             print("You will make a rule for a set of words.  For example, your \nwords must all have a double letter in them (like 'Green' and 'Door')\nin order to be classified as 'correct.' \nThis rule can deal with the number of or position of letters, \nthe presence of a specific letter, whether something is a \nvowel or consonant, or the part of speech of the word.  \nThe computer will try to guess your rule based on the words\nyou enter that follow the rule.  Have fun!\n")
 
     def get_y_words(self, n):
+        '''Asks the user for n words that follow the rule'''
         i = 1
         print("Enter %d words that belong in your set." % n)
         while i <= n:
@@ -42,6 +43,7 @@ class Game:
                 self.train_list.append((y_word, 'y'))
 
     def get_n_words(self, n):
+        '''Asks the user for n words that do not follow their rule.'''
         i = 1
         print("Now, enter %d words that do not belong in your set." % n)
         while i <= n:
@@ -58,6 +60,7 @@ class Game:
                 self.train_list.append((n_word, 'n'))
 
     def nltk_word(self):
+        '''Makes a list of nltk words from the Brown corpus that we can cycle through later.'''
         self.words = nltk.corpus.brown.words('ch01')
         numstrings = [str(i+1) for i in range(10, 30)]
         for num in numstrings: 
@@ -68,7 +71,6 @@ class Game:
         Finds word in self.words that matches the rule and returns that word, 
         deleting it and everything before it from the list.
         '''
-        #cycle through list and find a word that matches the best rule currently given by rule wizard
         index = 0
         word = ''
         for i in range(len(self.words)):
@@ -81,12 +83,17 @@ class Game:
         return word
 
     def word_guess(self, count):
+        '''Finds a word in the list of nltk words we made that it thinks follows the rules, asks the 
+        user whether it is correct, and continues until it gets 5 in a row correct. If incorrect, 
+        asks the user to supply two more correct and incorrect words. 
+        '''
         #cycle through an nltk corpus until you find a word that matches the rule and return that.
         word_to_guess = self.word_match() 
         correct = raw_input("Does the word \"%s\" follow your rule? (y/n) " % word_to_guess).lower()[0]
         if count == 5 and correct == 'y':
             print("Looks like I've got it figured out!")
             self.wizard.call_dat_shiznat()
+            return True
         elif correct == 'y': 
             self.guessed_words.append(word_to_guess.lower())
             self.word_guess(count+1)
@@ -115,6 +122,13 @@ class Game:
     def best_feature(self):
         return self.most_likely_features[0][0]
 
+    def play_again(self): 
+        '''Asks user if they want to play again.'''
+        if input("Do you want to play again? (y/n) ").lower()[0] == 'y':
+            return True
+        else: 
+            return False
+
     def test(self):
         train_list = [('poppies', 'y'), ('racket', 'n'),
                   ('somethin', 'n'), ('puppies', 'y'),
@@ -125,6 +139,12 @@ class Game:
         self.wizard = RuleWizard(train_list)
         self.wizard.find_best_rules()
         self.word_guess(0)
+        while self.play_again:
+            #Do we need the other functions in here? 
+            self.word_guess(0)
+            self.play_again
+
+
 
 instance = Game()
 instance.interaction_loop()
